8d98d585121688492b09f728148f3af14ba29af9
jhsu98 <jhsu98@gmail.com>
2020-02-07 09:54:00 -0500

    edited about me text

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -88,7 +88,7 @@
           </div>
           <div
             class="flex-1 text-lg sm:text-xl ml-6"
-          >For my "day job" I work as a Customer Success professional co-creating data collection solutions for SMB and enterprise companies. You can also find me blogging on Medium.com, on Battle.net playing Starcraft II, or in the dojo practicing martial arts.</div>
+          >For my "day job" I work as a Customer Success professional co-creating data collection solutions for SMB and enterprise companies. You can also find me blogging on Medium.com, playing Starcraft II, or in the dojo practicing martial arts.</div>
         </div>
 
         <!-- <div class="responsive-container">
