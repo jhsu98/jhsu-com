83736dce0f5b83dd3824d55a77dd21dd7ca031ef
jhsu98 <jhsu98@gmail.com>
2020-02-08 23:21:06 -0500

    changed 404 color and updated about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,8 +2,8 @@
   <Layout>
       <!-- PROFESSIONAL PROFILE -->
       <div class="overflow-x-hidden border-gray-200">
-        <div class="container-inner mx-auto text-xl pt-16 pb-6 mb-8 relative">
-          <h2 class="font-bold mb-6" id="about">Who am I?</h2>
+        <div class="container-inner mx-auto text-xl pt-12 pb-4 mb-8 relative">
+          <h2 class="font-bold mb-6" id="about">Some men dream of fortunes...</h2>
 
           <div class="absolute left-0" style="bottom: 10px; transform: translateX(-100%)">
             <svg width="170px" height="170px">
@@ -11,16 +11,16 @@
             </svg>
           </div>
 
-        <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
+        <div class="absolute right-0" style="top: 25px; transform: translate(100%) rotate(180deg)">
           <svg width="170px" height="170px">
             <use xlink:href="#dots-triangle" />
           </svg>
         </div>
 
-          <div class="flex flex-col sm:flex-row justify-between items-center mb-16">
+          <div class="flex flex-col sm:flex-row justify-between items-center mb-12">
             <div
               class="flex-1 text-lg sm:text-xl ml-6"
-            >For my "day job" I work as a Customer Success professional co-creating data collection solutions for SMB and enterprise companies. You can also find me blogging on Medium.com, playing Starcraft II, or in the dojo practicing martial arts.</div>
+            >I'm passionate about helping others achieve their goals; first through martial arts and now working with SMB & enterprise organizations on developing data-driven workflows. My goal is to continue empowering those around me and solve complex problems through leadership, creativity, and a critical pragmatism.</div>
           </div>
         </div>
       </div>
@@ -28,7 +28,7 @@
 
       <!-- PROFESSIONAL EXPERIENCE -->
       <div class="overflow-x-hidden border-gray-200 bg-background-primary">
-        <div class="container-inner mx-auto text-xl mt-8 relative">
+        <div class="container-inner mx-auto text-xl mt-6 relative">
           <h2 class="font-bold mb-6" id="about">Profesional Experience</h2>
           <div class="flex flex-col sm:flex-row justify-between items-center mb-8">
             <div class="flex-1 text-lg sm:text-xl ml-6">
@@ -44,13 +44,26 @@
                     </ul>
                 </li>
                 <li class="my-6">
-                  <b>Freelance Writer <span class="text-gray-500">(2018-Present)</span></b><br />ASDF
+                  <b>Freelance Writer <span class="text-gray-500">(2018-Present)</span></b>
+                  <ul class="pl-6" style="list-style-type: circle;">
+                    <li>Programming tutorials published by Better Programming on Medium.com</li>
+                    <li>Writing advice published by Better Marketing on Medium.com</li>
+                  </ul>
                 </li>
                 <li class="my-6">
-                  <b>Adjunct Professor, Undergraduate Programming <span class="text-gray-500">(2010-2016)</span></b><br />ASDF
+                  <b>Adjunct Professor, Undergraduate Programming <span class="text-gray-500">(2010-2016)</span></b>
+                  <ul class="pl-6" style="list-style-type: circle;">
+                    <li>IT 102: Discrete Structures, discrete mathematics with programming fundamentals in Python</li>
+                    <li>IT 207: Applied IT Programming, web-focused server-side programming with PHP, MySQL, and HTML</li>
+                    </ul>
                 </li>
                 <li class="my-6">
-                  <strong>Martial Arts Instructor <span class="text-gray-500">(1997-2017)</span></strong><br />ASDF
+                  <strong>Martial Arts Instructor <span class="text-gray-500">(1997-2017)</span></strong>
+                  <ul class="pl-6" style="list-style-type: circle;">
+                    <li>Hired, trained, and supervised junior instructors</li>
+                    <li>Community role model, leadership, and student advocacy</li>
+                    <li>Over 20,000 hours of experience teaching</li>
+                    </ul>
                 </li>
               </ul>
             </div>
@@ -61,9 +74,9 @@
 
       <!-- EDUCATION -->
       <div class="overflow-x-hidden border-gray-200 bg-background-primary">
-        <div class="container-inner mx-auto text-xl mt-8 relative">
+        <div class="container-inner mx-auto text-xl mt-2 relative">
           <h2 class="font-bold mb-6" id="about">Education</h2>
-          <div class="flex flex-col sm:flex-row justify-between items-center mb-8">
+          <div class="flex flex-col sm:flex-row justify-between items-center mb-4">
             <div class="flex-1 text-lg sm:text-xl ml-6">
               I was raised with education being paramount. I am proud to hold three degrees that have shaped my expertise and form a unique blend of technical, leadership, and communication skills.
               <ul class="text-lg sm:text-xl mx-6">
@@ -85,8 +98,8 @@
 
       <!-- PERSONAL INTERESTS -->
       <div class="overflow-x-hidden border-gray-200">
-        <div class="container-inner mx-auto text-xl pt-16 pb-6 mb-8 relative">
-          <h2 class="font-bold mb-6" id="about">Personal Interests</h2>
+        <div class="container-inner mx-auto text-xl pt-12 pb-4 mb-8 relative">
+          <h2 class="font-bold mb-6" id="about">Others dream of cookies...</h2>
 
           <div class="absolute left-0" style="bottom: 10px; transform: translateX(-100%)">
             <svg width="170px" height="170px">
@@ -94,7 +107,7 @@
             </svg>
           </div>
 
-        <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
+        <div class="absolute right-0" style="top: 25px; transform: translate(100%) rotate(180deg)">
           <svg width="170px" height="170px">
             <use xlink:href="#dots-triangle" />
           </svg>
@@ -103,7 +116,7 @@
           <div class="flex flex-col sm:flex-row justify-between items-center mb-16">
             <div
               class="flex-1 text-lg sm:text-xl ml-6"
-            >For my "day job" I work as a Customer Success professional co-creating data collection solutions for SMB and enterprise companies. You can also find me blogging on Medium.com, playing Starcraft II, or in the dojo practicing martial arts.</div>
+            >I'm a lifelong learner and martial artist. I'm energized by learning new technologies and I embrace the grind of developing a new skill. I relax by building PCs, watching mixed martial arts, and competing in anything and everything.</div>
           </div>
         </div>
       </div>
